71f66f8d7beb70a648a4555425fca0324090cdde
Arsalaan☪︎☪︎☪︎ <arsalaan@Arsalaans-MacBook-Air.local>
2026-01-13 18:44:40 +0530

    added @ConfigurationProperties annotation on BuildInfo class

--- src/main/java/com/config/config/BuildInfoController.java
+++ src/main/java/com/config/config/BuildInfoController.java
@@ -1,23 +1,27 @@
 package com.config.config;
 
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class BuildInfoController {
 
-    @Value("${build.id:default}")
-    private String buildId;
+//    @Value("${build.id:default}")
+//    private String buildId;
+//
+//    @Value("${build.version:default}")
+//    private String buildVersion;
+//
+//    @Value("${build.name:default}")
+//    private String buildName;
 
-    @Value("${build.version:default}")
-    private String buildVersion;
-
-    @Value("${build.name:default}")
-    private String buildName;
+    private BuildInfo buildInfo;
 
     @GetMapping("/build-info")
     public String getBuildInfo(){
-        return "BuildID:-"+buildId+" | "+"BuildName:-"+buildName+" | "+"BuildVersion:-"+buildVersion;
+        return "BuildID:-"+buildInfo.getId()+" | "+"BuildName:-"+buildInfo.getName()+" | "+"BuildVersion:-"+buildInfo.getVersion();
     }
 }
